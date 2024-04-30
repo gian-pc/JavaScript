92f19921221b2de968d69e382572acb3f02aebf0
Gian Paucar Cortez <gpaucarcortez@gmail.com>
2024-04-30 18:55:48 -0500

    01-const-let

---
+++ 01-const-let.js
@@ -0,0 +1,18 @@
+// Variables y Constantes
+
+const nombre = 'Gian';
+const apellido = 'Paucar';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var no se debe de usar...
+
+if (true) {
+    const nombre = 'Peter';
+    console.log(nombre);
+}   
+
+console.log(valorDado);
